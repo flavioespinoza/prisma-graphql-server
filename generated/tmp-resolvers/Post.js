9927856196cb5b56756c3073c8e5b59fad0d1c13
flavioespinoza <flavio.espinoza@gmail.com>
2019-06-22 07:02:18 -0600

    fixed_build

--- generated/tmp-resolvers/Post.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
-// Please do not import this file directly but copy & paste to your application code.
-Object.defineProperty(exports, "__esModule", { value: true });
-const graphqlgen_1 = require("../graphqlgen");
-exports.Post = Object.assign({}, graphqlgen_1.PostResolvers.defaultResolvers, { author: (parent, args, ctx) => {
-        throw new Error("Resolver not implemented");
-    } });
